bf21c9d6f2b5d913d3fccfeae04aa6001dd876b0
freyes <freyes>
2021-02-17 10:40:01 -0500

    Versión final

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AppState } from '../../app.reducer';
+import { Store } from '@ngrx/store';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-navbar',
@@ -6,11 +10,25 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+
+  nombreUsuario: string;
+  userSubscription: Subscription;
+
+  constructor(private store: Store<AppState>) { }
+
+
+  ngOnDestroy(): void {
+    this.userSubscription.unsubscribe();
+  }
 
   ngOnInit(): void {
+    this.userSubscription = this.store.select('auth')
+      .pipe(
+        filter(({user}) => user != null)
+      )
+      .subscribe(({ user }) => this.nombreUsuario = user.nombre);
   }
 
 }
